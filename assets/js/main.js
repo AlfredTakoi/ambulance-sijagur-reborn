de9a97f5dfb0650e02d19d71d62f7cbd30bad359
AlfredTakoi <alfredtakoi@gmail.com>
2023-06-04 00:25:06 +0700

    update font size contact and update navbar toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.navbar-toggle').click(function(e){
         $('.navbar-mobile').toggleClass('translate-x-[475px]')
-        $(this).find('span:nth-child(1)').toggleClass('-translate-y-1.5 rotate-45')
+        $(this).find('span:nth-child(1)').toggleClass('-translate-y-2.5 rotate-45')
         $(this).find('span:nth-child(2)').toggleClass('opacity-0')
-        $(this).find('span:nth-child(3)').toggleClass('translate-y-1.5 -rotate-45')
+        $(this).find('span:nth-child(3)').toggleClass('translate-y-2.5 -rotate-45')
         e.stopPropagation()
     })
 
@@ -13,17 +13,17 @@ $(document).ready(function(){
 
     $(document).click(function() {
         $('.navbar-mobile').addClass('translate-x-[475px]')
-        $(this).find('span:nth-child(1)').addClass('-translate-y-1.5').removeClass('rotate-45')
+        $(this).find('span:nth-child(1)').addClass('-translate-y-2.5').removeClass('rotate-45')
         $(this).find('span:nth-child(2)').removeClass('opacity-0')
-        $(this).find('span:nth-child(3)').addClass('translate-y-1.5').removeClass('-rotate-45') 
+        $(this).find('span:nth-child(3)').addClass('translate-y-2.5').removeClass('-rotate-45') 
     });
 
     $('.mobile-navbar-link').each(function(){
         $(this).click(function(){
             $('.navbar-mobile').addClass('translate-x-[475px]')
-            $('.navbar-toggle').find('span:nth-child(1)').addClass('-translate-y-1.5').removeClass('rotate-45')
+            $('.navbar-toggle').find('span:nth-child(1)').addClass('-translate-y-2.5').removeClass('rotate-45')
             $('.navbar-toggle').find('span:nth-child(2)').removeClass('opacity-0')
-            $('.navbar-toggle').find('span:nth-child(3)').addClass('translate-y-1.5').removeClass('-rotate-45') 
+            $('.navbar-toggle').find('span:nth-child(3)').addClass('translate-y-2.5').removeClass('-rotate-45') 
         })
     })
 })
