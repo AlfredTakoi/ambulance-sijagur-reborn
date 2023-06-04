5c16307cf926c1a15dbef62fad7f719eeb2fc5e2
AlfredTakoi <alfredtakoi@gmail.com>
2023-06-04 13:26:41 +0700

    fix overlay dispaly

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.navbar-toggle').click(function(e){
         $('.navbar-mobile').toggleClass('translate-x-[475px]')
-        $('.overlay').toggleClass('opacity-0 opacity-70')
+        $('.overlay').fadeToggle(700)
         $(this).find('span:nth-child(1)').toggleClass('-translate-y-2.5 rotate-45')
         $(this).find('span:nth-child(2)').toggleClass('opacity-0')
         $(this).find('span:nth-child(3)').toggleClass('translate-y-2.5 -rotate-45')
@@ -15,8 +15,8 @@ $(document).ready(function(){
     $(document).click(function() {
         $('.navbar-mobile').addClass('translate-x-[475px]')
         $(this).find('span:nth-child(1)').addClass('-translate-y-2.5').removeClass('rotate-45')
-        $('.overlay').addClass('opacity-0').removeClass('opacity-70')
         $(this).find('span:nth-child(2)').removeClass('opacity-0')
+        $('.overlay').fadeOut(700)
         $(this).find('span:nth-child(3)').addClass('translate-y-2.5').removeClass('-rotate-45') 
     });
 
@@ -24,8 +24,8 @@ $(document).ready(function(){
         $(this).click(function(){
             $('.navbar-mobile').addClass('translate-x-[475px]')
             $('.navbar-toggle').find('span:nth-child(1)').addClass('-translate-y-2.5').removeClass('rotate-45')
-            $('.navbar-toggle').find('span:nth-child(2)').removeClass('opacity-0')
             $('.overlay').addClass('opacity-0 ').removeClass('opacity-70')
+            $('.overlay').fadeOut(700)
             $('.navbar-toggle').find('span:nth-child(3)').addClass('translate-y-2.5').removeClass('-rotate-45') 
         })
     })
